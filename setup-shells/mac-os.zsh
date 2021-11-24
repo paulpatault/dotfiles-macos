047f8c91d55cb761217555b89034f9faa90b081d
paulpatault <p.patault@gmail.com>
2021-11-24 16:59:21 +0100

    macos: update settings

--- setup-shells/mac-os.zsh
+++ setup-shells/mac-os.zsh
@@ -9,7 +9,7 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 # répétition des caratères au lieu de l'affichage des accents
 defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
 
-# affichage des • des apps ouvertes
+# affichage des • des apps ouvertes
 defaults write com.apple.dock show-process-indicators -bool true
 
 # taille du Dock
@@ -17,6 +17,10 @@ defaults write com.apple.dock tilesize -int 49
 
 # effet de minimisation des apps
 defaults write com.apple.dock mineffect -string "genie"
+
+# dock se cache
+defaults write com.apple.dock autohide -bool "true"
+defaults write com.apple.dock show-recents -bool false
 
 # repetition de touche
 defaults write NSGlobalDomain KeyRepeat -int 1
@@ -42,12 +46,23 @@ defaults write com.apple.finder ShowStatusBar -bool true
 defaults write NSGlobalDomain AppleShowAllExtensions -bool true
 
 # style colonne finder
-defaults write com.apple.Finder FXPreferredViewStyle clmv
+defaults write com.apple.Finder FXPreferredViewStyle "clmv"
+
+# garde les dossiers en haut
+defaults write com.apple.finder _FXSortFoldersFirst -bool true
+
+# Show icons for hard drives, servers, and removable media on the desktop
+defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
+defaults write com.apple.finder ShowHardDrivesOnDesktop -bool false
+defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
+
+# Avoid creating .DS_Store files on network volumes
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
 
 # dossier pour capture d'écran
-
 mkdir ~/Documents/Screenshots
 defaults write com.apple.screencapture location ~/Documents/Screenshots
+
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # KILLALL
 
