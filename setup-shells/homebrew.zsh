b669b10492a60bfe811c83411a51a5054c965b28
paulpatault <p.patault@gmail.com>
2022-01-12 17:11:51 +0100

    maj opam installation

--- setup-shells/homebrew.zsh
+++ setup-shells/homebrew.zsh
@@ -1,7 +1,6 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 BREW_MIN=(
-opam
 wget
 rg
 fd
--- setup-shells/opam.zsh
+++ setup-shells/opam.zsh
@@ -1,9 +1,11 @@
 OPAM_MIN=(
-utop
+ocaml
 ocaml-lsp-server
-merlin
 menhir
 dune
+merlin
+ocamlfind
+utop
 )
 
 OPAM_ALL=(
@@ -11,13 +13,16 @@ alt-ergo
 coq
 core
 graphics
-ocamlfind
 ocaml-system
 why3
 why3-ide
 )
 
+bash -c "sh <(curl -fsSL https://raw.githubusercontent.com/ocaml/opam/master/shell/install.sh)"
+
 opam init
+
+eval $(opam env --switch=default)
 
 if [[ $1 ]]
 then
