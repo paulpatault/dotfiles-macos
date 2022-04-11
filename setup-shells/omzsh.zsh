3f495d374914e62b6968e077d861ea1ce8deb31f
paulpatault <p.patault@gmail.com>
2022-04-11 16:06:02 +0200

    clean

--- setup-shells/omzsh.zsh
+++ setup-shells/omzsh.zsh
@@ -8,4 +8,3 @@ git clone https://github.com/sindresorhus/pure.git "$HOME/.zsh/pure"
 
 git clone --depth 1 https://github.com/dexpota/kitty-themes.git ~/.config/kitty/kitty-themes
 ln -f -s ~/.config/kitty/themes/gruvbox.conf ~/.config/kitty/theme.conf
-
