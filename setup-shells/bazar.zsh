4233782c4502babcf3035eb84d24537d8a7ee226
Paul Patault <43556190+paulpatault@users.noreply.github.com>
2022-06-22 13:05:03 +0200

    add lsp lua config

--- setup-shells/bazar.zsh
+++ setup-shells/bazar.zsh
@@ -1,3 +1,4 @@
 pip3 install neovim
 npm install neovim
 npm i -g vscode-langservers-extracted
+git clone --depth 1 https://github.com/sumneko/lua-language-server.git $HOME/dotfiles/config/lua-language-server
