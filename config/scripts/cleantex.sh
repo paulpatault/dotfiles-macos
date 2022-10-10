c08ed3736b8fbef725b7b8e354596aa9c9febb1b
Paul Patault <43556190+paulpatault@users.noreply.github.com>
2022-10-10 10:41:03 +0200

    add cleantex

---
+++ config/scripts/cleantex.sh
@@ -0,0 +1,14 @@
+rm -rf \
+    *.aux \
+    *.fdb_latexmk \
+    *.fls \
+    *.log \
+    *.synctex.gz \
+    *.bbl \
+    *.bcf \
+    *.blg \
+    *.fdb_latexmk \
+    *.out \
+    *.run.xml \
+    _minted-* \
+    .DS_Store
