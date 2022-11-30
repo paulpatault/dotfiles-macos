df1ed8ec69ed5041aa97b570f8457fce4ce90f91
Paul Patault <43556190+paulpatault@users.noreply.github.com>
2022-11-30 14:51:59 +0100

    add utoprc

--- config/utop/init.ml
+++ config/utop/init.ml
@@ -1,2 +1,3 @@
 #edit_mode_vi
-#utop_prompt_fancy_light
+(* #utop_prompt_fancy_light *)
+#utop_prompt_dummy
--- setup-shells/symlinks.zsh
+++ setup-shells/symlinks.zsh
@@ -1,3 +1,4 @@
 ln -s $HOME/dotfiles/config/gitconfig $HOME/.gitconfig
 ln -s $HOME/dotfiles/config/zshrc $HOME/.zshrc
 ln -s $HOME/dotfiles/config/ $HOME/.config
+ln -s $HOME/dotfiles/config/utop/utoprc $HOME/.utoprc
