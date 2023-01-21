82dd76b55485901226e1dea78d5244cc7f25b365
Paul Patault <43556190+paulpatault@users.noreply.github.com>
2023-01-21 13:37:53 +0100

    fix coq

--- config/nvim/ftplugin/coq.lua
+++ config/nvim/ftplugin/coq.lua
@@ -1,7 +1,5 @@
-vim.keymap.set("n", "s", vim.cmd("CoqNext")
-vim.keymap.set("n", "S", vim.cmd("CoqUndo")
-
-vim.keymap.set("n", "<down>",  "<Plug>CoqNext")
-vim.keymap.set("n", "<up>",    "<Plug>CoqUndo")
-vim.keymap.set("n", "<right>", "<Plug>CoqToLine")
--- vim.keymap.set("n", "<left>",  "<Plug>CoqToTop")
+vim.keymap.set("n", "<down>", "<Plug>CoqNext")
+vim.keymap.set("n", "<up>",   "<Plug>CoqUndo")
+vim.keymap.set("n", "s", "<Plug>CoqNext")
+vim.keymap.set("n", "S", "<Plug>CoqUndo")
+vim.keymap.set("n", "L", "<Plug>CoqToLine")
