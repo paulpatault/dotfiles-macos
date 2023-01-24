b54e797564f720cf5f93f4d76872dacda2e3e105
Paul Patault <43556190+paulpatault@users.noreply.github.com>
2023-01-24 01:25:27 +0100

    coq color

--- config/nvim/lua/ppatault/theme.lua
+++ config/nvim/lua/ppatault/theme.lua
@@ -8,7 +8,7 @@ vim.g.gruvbox_invert_selection = false
 if (vim.api.nvim_get_var("the_bg_is_dark") == 1) then
   vim.opt.background = "dark"
   vim.api.nvim_set_var("coq_sent_color", "#574300")
-  vim.api.nvim_set_var("coq_check_color", "#34463e")
+  vim.api.nvim_set_var("coq_check_color", "#30423c")
   vim.cmd.colorscheme("gruvbox")
 else
   vim.opt.background = "light"
