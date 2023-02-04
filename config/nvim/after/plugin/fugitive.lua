8081b558aa47d7a7d319812cd81f042c71f15de3
Paul Patault <43556190+paulpatault@users.noreply.github.com>
2023-02-04 19:57:11 +0100

    fugitive

--- config/nvim/after/plugin/fugitive.lua
+++ config/nvim/after/plugin/fugitive.lua
@@ -3,8 +3,7 @@ vim.keymap.set("n", "<leader>gb", function() vim.cmd.Git("blame") end, { desc = 
 
 local fugitive_grp = vim.api.nvim_create_augroup("fugitive_grp", {clear = true})
 
-local autocmd = vim.api.nvim_create_autocmd
-autocmd("BufWinEnter", {
+vim.api.nvim_create_autocmd("BufWinEnter", {
     group = fugitive_grp,
     pattern = "*",
     callback = function()
@@ -15,18 +14,19 @@ autocmd("BufWinEnter", {
         local bufnr = vim.api.nvim_get_current_buf()
         local opts = {buffer = bufnr, remap = false}
         vim.keymap.set("n", "<leader>p", function()
-            vim.cmd.Git('push')
+            vim.cmd.Git("push")
             vim.cmd.quit()
         end, opts)
 
         vim.keymap.set("n", "<leader>f", function()
-            vim.cmd.Git('push --force-with-lease')
+            vim.cmd.Git("push --force-with-lease")
+            vim.cmd.quit()
         end, opts)
 
-        --[[ -- rebase always
+        -- rebase always
         vim.keymap.set("n", "<leader>P", function()
-            vim.cmd.Git({'pull',  '--rebase'})
-        end, opts) ]]
+            vim.cmd.Git({"pull",  "--rebase=true"})
+        end, opts)
 
         --[[ -- NOTE: It allows me to easily set the branch i am pushing and any tracking
         -- needed if i did not set the branch up correctly
