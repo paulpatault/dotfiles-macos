6a4667ac95032957ae730a19eafa594288dbda6b
Paul Patault <43556190+paulpatault@users.noreply.github.com>
2023-02-04 19:57:26 +0100

    cleanup ocaml

--- config/nvim/ftplugin/ocaml.lua
+++ config/nvim/ftplugin/ocaml.lua
@@ -1,12 +1,59 @@
-vim.keymap.set("n", "<localleader>af", "a assert false<esc>")
--- vim.keymap.set("n", "<leader>aft", "a assert false (* TODO *)<esc>")
-vim.keymap.set("n", "<leader>d", [[i<cr><esc>kaif debug then Format.eprintf "%a@." ;<esc>i]])
+local opam_share = os.getenv("OPAM_SWITCH_PREFIX") .. "/share"
 
-vim.keymap.set("n", "s", function() vim.call("OCaml_switch", 0) end)
-vim.keymap.set("i", "<c-a>", " assert false")
+vim.api.nvim_set_var("python_host_prog", "/usr/bin/python")
+vim.api.nvim_set_var("python3_host_prog", "/opt/homebrew/bin/python3")
+vim.api.nvim_set_var("merlin_python_version", 3)
 
-vim.keymap.set("n", "<localleader>f", function() vim.cmd("FloatermNew utop") end)
-vim.keymap.set("n", "<localleader>i", [[:!ocaml %<cr>]])
+vim.api.nvim_set_var("opamshare", opam_share)
+
+vim.opt.rtp:append(opam_share .. "/merlin/vim/doc")
+vim.opt.rtp:append(opam_share .. "/merlin/vim")
+vim.opt.rtp:append(opam_share .. "/merlin/vimbufsync")
+vim.opt.rtp:append(opam_share .. "/ocp-indent/vim")
+
+vim.api.nvim_create_autocmd( "BufWinEnter", {
+    group = vim.api.nvim_create_augroup("ocaml_grp", {clear = true}),
+    pattern = { "*.ml", "*.mli" }, -- "ocaml_interface"
+    callback = function ()
+
+      if vim.bo.ft ~= "ocaml" then return end
+
+      vim.cmd("unlet b:did_indent")
+
+      vim.cmd.source(opam_share .. "/ocp-indent/vim/indent/ocaml.vim")
+      vim.opt.iskeyword:append("_")
+
+      -- insert code
+      vim.keymap.set("n", "<localleader>af", "a assert false<esc>")
+      -- vim.keymap.set("n", "<leader>d", [[i<cr><esc>kaif debug then Format.eprintf "%a@." ;<esc>i]])
+      vim.keymap.set("n", "<localleader>f", function() vim.cmd("FloatermNew utop") end)
+      vim.keymap.set("n", "<localleader>i", function() vim.cmd("!ocaml %") end)
+
+      -- switch
+      vim.keymap.set("n", "s", function() vim.call("OCaml_switch", 0) end)
+      vim.keymap.set("n", "S", function() vim.call("OCaml_switch", 1) end)
+
+      vim.keymap.set("i", "<c-a>", " assert false")
+
+    end
+})
+
+-- nvim_set_var("ocaml_noend_error", 1)
+-- nvim_set_var("ocaml_revised", 1)
+
+vim.g.neoformat_ocaml_ocamlformat = {
+  exe = "ocamlformat",
+  no_append = true,
+  stdin = true,
+  args = {
+    "--disable-outside-detected-project",
+    "--name",
+    [[%:p"]],
+    "-"
+  }
+}
+
+vim.g.neoformat_enabled_ocaml = {"ocamlformat"}
 
 --[[ vim.keymap.set("n", "ml", function()
   local filename = vim.fn.expand("%:t:r")
--- config/nvim/lua/ppatault/init.lua
+++ config/nvim/lua/ppatault/init.lua
@@ -7,52 +7,51 @@ require("ppatault.globals")
 -- require("rust-tools").setup({})
 require("ppatault.highlights")
 require("ppatault.theme")
-require("ppatault.ocaml")
 
 local fts = {
   zsh = "sh",
   mlw = "why3",
-  mp = "patmat",
+  mp  = "patmat",
   lus = "lustre",
   ept = "lustre",
-  proof = "proof",
   mll = "ocaml.ocamllex",
-  --mly = "ocaml.menhir",
   mli = "ocaml.interface",
-  fx = "scala.no_lsp",
+  fx  = "scala.no_lsp",
+  proof = "proof",
+  --mly = "ocaml.menhir",
 }
 
-local iopt_grp = vim.api.nvim_create_augroup("Opts_grp", {clear = true})
+local options_group = vim.api.nvim_create_augroup("OptionsGroup", {clear = true})
 
 for k, v in pairs(fts) do
   vim.api.nvim_create_autocmd("BufRead,BufNewFile",
     {
       pattern = "*." .. k,
       command = "set filetype=" .. v,
-      group = iopt_grp,
+      group = options_group,
     }
   )
 end
 
-vim.api.nvim_create_autocmd("FileType",
-  {
-    pattern = {"c","latex","cpp"},
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = { "c", "latex", "cpp"},
     command = "set sw=4",
-    group   = iopt_grp
+    group   = options_group
   })
-vim.api.nvim_create_autocmd("FileType",
-  {
-    pattern = {"markdown","lua","kawa","ocaml","wh3","lustre"},
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = { "markdown", "lua", "kawa", "ocaml", "wh3", "lustre"},
     command = "set sw=2",
-    group   = iopt_grp
+    group   = options_group
   })
 vim.api.nvim_create_autocmd("FileType",
   {
     pattern = {"*.tex"},
     command = "setlocal spell",
-    group   = iopt_grp
+    group   = options_group
   })
 
+
+-- lsp loading
 local ok, res = pcall(function() require("ppatault.lsp") end)
 
 if not ok then
