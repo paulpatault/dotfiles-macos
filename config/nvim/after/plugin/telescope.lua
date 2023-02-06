8f80d6409285689ff076d0ea4f3af06264e3bdf3
Paul Patault <43556190+paulpatault@users.noreply.github.com>
2023-02-06 10:39:00 +0100

    telescope defaults

--- config/nvim/after/plugin/telescope.lua
+++ config/nvim/after/plugin/telescope.lua
@@ -1,9 +1,9 @@
 
 local builtin = require("telescope.builtin")
-vim.keymap.set("n", "<leader>pf",  builtin.find_files, { desc = "p + [F]iles" })
-vim.keymap.set("n", "<leader>ph",  builtin.help_tags, { desc = "p + [H]elp tags" })
+vim.keymap.set("n", "<leader>pf", builtin.find_files, { desc = "p + [F]iles" })
+vim.keymap.set("n", "<leader>ph", builtin.help_tags, { desc = "p + [H]elp tags" })
 vim.keymap.set("n", "<leader>pg", builtin.git_files, { desc = "p + [G]it files" })
-vim.keymap.set("n", "<leader>pb",  builtin.buffers, { desc = "p + [B]uffer" })
+vim.keymap.set("n", "<leader>pb", builtin.buffers, { desc = "p + [B]uffer" })
 vim.keymap.set("n", "<leader>pd", builtin.diagnostics, { desc = "p + [D]iagnostics" })
 vim.keymap.set("n", "<leader>pr", builtin.live_grep, { desc = "p + [R]ipgrep" })
 vim.keymap.set("n", "<leader>pk", builtin.keymaps, { desc = "p + [K]eymaps" })
@@ -26,57 +26,67 @@ require('plenary.filetype').add_file({
   ['lus'] = 'lustre',
   ['ept'] = 'lustre'})
 
+  --[[ vimgrep_arguments = { 'fzf' }, ]]
+  --
 telescope.setup({
-  defaults = {
-    --[[ vimgrep_arguments = {
-      'fzf'
-    }, ]]
-    -- theme = "get_ivy",
-    file_ignore_patterns = {
-      [[undo/%.*]],
-      [[_build/*]],
-      [[%.key]],
-      [[%.agdai]],
-      [[_opam/*]],
-      [[%.cmo]],
-      [[%.cmi]],
-      [[%.cmx]],
-      [[%.o]],
-      [[%.gz]],
-      [[%.cmt]],
-      [[%.cmti]],
-      [[%.pyc]],
-      [[%.aux]],
-      [[%.jpg]],
-      [[%.png]],
-      [[%.aux]],
-      [[%.fdb_latexmk]],
-      [[%.fls]],
-      [[%.log]],
-      [[%.synctex.gz]],
-      [[%.bbl]],
-      [[%.bcf]],
-      [[%.blg]],
-      [[%.out]],
-      [[%.toc]],
-      [[%.vrb]],
-      [[%.run.xml]],
-      [[%.pdf]]},
+    pickers = {
+      find_files  = { theme = "dropdown" },
+      help_tags   = { theme = "dropdown" },
+      git_files   = { theme = "dropdown" },
+      buffers     = { theme = "dropdown" },
+      diagnostics = { theme = "dropdown" },
+      live_grep   = { theme = "dropdown" },
+      keymaps     = { theme = "dropdown" },
+      quickfix    = { theme = "dropdown" },
+    },
+    defaults = {
+      file_ignore_patterns = {
+        [[undo/%.*]],
+        [[_build/*]],
+        [[%.key]],
+        [[%.agdai]],
+        [[_opam/*]],
+        [[%.cmo]],
+        [[%.cmi]],
+        [[%.cmx]],
+        [[%.o]],
+        [[%.gz]],
+        [[%.cmt]],
+        [[%.cmti]],
+        [[%.pyc]],
+        [[%.aux]],
+        [[%.jpg]],
+        [[%.png]],
+        [[%.aux]],
+        [[%.fdb_latexmk]],
+        [[%.fls]],
+        [[%.log]],
+        [[%.synctex.gz]],
+        [[%.bbl]],
+        [[%.bcf]],
+        [[%.blg]],
+        [[%.out]],
+        [[%.toc]],
+        [[%.vrb]],
+        [[%.run.xml]],
+        [[%.pdf]]},
 
-    -- layout_config = {prompt_position = "bottom"},
+    -- layout_config = {
+      -- prompt_position = "bottom",
+    -- },
 
     file_sorter = require("telescope.sorters").get_fzy_sorter,
     file_previewer = require("telescope.previewers").vim_buffer_cat.new,
     grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
     qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
 
-    extensions = {
-      fzy_native = {
-        override_generic_sorter = false,
-        override_file_sorter = true
-      }
-    },
-  }
+  },
+  extensions = {
+    fzy_native = {
+      override_generic_sorter = false,
+      override_file_sorter = true
+    }
+  },
 })
 
 pcall(telescope.load_extension, "fzy_native")
