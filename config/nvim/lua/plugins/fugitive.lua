a27b77a2c97f8964019bc0f32a384b3f06dc3413
Paul Patault <43556190+paulpatault@users.noreply.github.com>
2023-04-04 14:07:45 +0200

    fugitive + env

--- config/nvim/lua/plugins/fugitive.lua
+++ config/nvim/lua/plugins/fugitive.lua
@@ -16,7 +16,8 @@ return {
             end
 
             local bufnr = vim.api.nvim_get_current_buf()
-            local opts = {buffer = bufnr, remap = false}
+            local opts = { buffer = bufnr, remap = false }
+
             vim.keymap.set("n", "<leader>p", function()
                 vim.cmd.Git("push")
                 vim.cmd.quit()
@@ -31,6 +32,8 @@ return {
             vim.keymap.set("n", "<leader>P", function()
                 vim.cmd.Git({"pull",  "--rebase=true"})
             end, opts)
+
+            vim.keymap.set("n", "q", function() vim.cmd.quit() end, opts)
 
         end,
     })
