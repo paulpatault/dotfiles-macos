b863410fdf13853b2823a7e06d25cf6d38f25bf8
Paul Patault <43556190+paulpatault@users.noreply.github.com>
2023-05-17 14:02:33 +0200

    down ft ocamlinit

--- setup-shells/symlinks.sh
+++ setup-shells/symlinks.sh
@@ -3,5 +3,6 @@
 ln -s $HOME/d/github/dotfiles/config/ $HOME/.config
 
 ln -s $HOME/.config/.gitconfig $HOME/.gitconfig
+ln -s $HOME/.config/.ocamlinit $HOME/.ocamlinit
 ln -s $HOME/.config/zshrc $HOME/.zshrc
 ln -s $HOME/.config/utop/utoprc $HOME/.utoprc
