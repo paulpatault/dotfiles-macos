a7a22797236dd1c93a9229d40649dbb7091ebfc9
Paul Patault <43556190+paulpatault@users.noreply.github.com>
2023-07-12 14:41:11 +0200

    tex map

--- config/nvim/ftplugin/tex.lua
+++ config/nvim/ftplugin/tex.lua
@@ -1,5 +1,6 @@
 vim.keymap.set("v", "<C-b>", "<Plug>(sandwich-add)ctextbf<cr>")
 vim.keymap.set("v", "<C-i>", "<Plug>(sandwich-add)ctextit<cr>")
+vim.keymap.set("v", "<C-e>", "<Plug>(sandwich-add)cemph<cr>")
 vim.keymap.set("v", "<C-t>", "<Plug>(sandwich-add)ctexttt<cr>")
 vim.keymap.set("n", "<leader>i", "i~$$<esc>i")
 
@@ -8,3 +9,5 @@ vim.keymap.set("n", "<localleader>T", "<Plug>(vimtex-toc-toggle)")
 vim.keymap.set("n", "<localleader>l", "<Plug>(vimtex-compile)")
 vim.keymap.set("n", "<localleader>e", "<Plug>(vimtex-errors)")
 vim.keymap.set("n", "<localleader>v", "<Plug>(vimtex-view)")
+
+vim.keymap.set("n", "<localleader>w", [[/\(\<\w\+\>\)\_s*\<\1\><cr>]])
