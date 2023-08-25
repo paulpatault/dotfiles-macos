df682b28382bd383df0e4665777b44ea5e9e494f
Paul Patault <43556190+paulpatault@users.noreply.github.com>
2023-08-25 10:56:33 +0200

    scripts

---
+++ config/scripts/ignore-tex.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+touch .gitignore
+
+echo "*.aux"          >> .gitignore
+echo "*.bbl"          >> .gitignore
+echo "*.bcf"          >> .gitignore
+echo "*.blg"          >> .gitignore
+echo "*.fdb_latexmk"  >> .gitignore
+echo "*.fls"          >> .gitignore
+echo "*.log"          >> .gitignore
+echo "*.nav"          >> .gitignore
+echo "*.out"          >> .gitignore
+echo "*.run.xml"      >> .gitignore
+echo "*.snm"          >> .gitignore
+echo "*.synctex.gz"   >> .gitignore
+echo "*.toc"          >> .gitignore
+echo "*.vrb"          >> .gitignore
+echo "*.xdv"          >> .gitignore
